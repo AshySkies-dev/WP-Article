2992205630daa6f95e48f0ed0975a97073f7a634
AshySkies-dev <aNeonaru@gmail.com>
2026-03-28 01:57:17 +0500

    Updated search.js

--- js/search.js
+++ js/search.js
@@ -1,35 +1,20 @@
-jQuery(function ($) {
-    $('#toggle_advanced_search').on('click', function() {
-        $('#advanced_search').toggle();
-    });
+jQuery(document).ready(function($) {
+    $('#ajax-search-form').on('submit', function(e) {
+        e.preventDefault();
 
-    function sendSearchRequest() {
-        var data = {
-            action: 'search', // ????????
-            text: $('#search_content').val(),
-            author: $('#search_author').val(),
-            institution: $('#search_institution').val(),
-            language: $('#search_language').val(),
-            year: $('#search_year').val(),
-        };
+        const formData = $(this).serialize();
+        const resultsContainer = $('#search-results');
 
-        $.post(ajax_object.ajaxurl, data, function(response) {
-            $('.articles-container').html(response);  // ????????
+        $.ajax({
+            url: '<?php echo admin_url("admin-ajax.php"); ?>',
+            type: 'POST',
+            data: formData,
+            beforeSend: function() {
+                resultsContainer.html('...');
+            },
+            success: function(data) {
+                resultsContainer.html(data);
+            }
         });
-    }
-
-    $('#search_run').on('click', function() {
-        sendSearchRequest();
-    });
-
-    $('#reset_filters').on('click', function () {
-        $('#advanced_search').find('input, select').val('');
-        $('.articles-container').html('');     // ????????
-        sendSearchRequest();
-    });
-
-    $('#search_content, #search_author, #search_institution, #search_language, #search_year').on('input change', function() {
-        console.log(ajax_object);
-        sendSearchRequest();
     });
 });
